2f3f14fa65fd0b4c318eede44e581e460ed77576
Naren404 <narendrasunar47@gmail.com>
2021-06-01 16:53:52 +0545

    logic polished

--- script/tictactoe.js
+++ script/tictactoe.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+
 	let count=0;
+
 	let player1_row_1 = 0;
 	let player1_row_2 = 0;
 	let player1_row_3 = 0;
-
 	let player1_col_1 = 0;
 	let player1_col_2 = 0;
 	let player1_col_3 = 0;
@@ -11,19 +12,20 @@ $(document).ready(function(){
 	let player2_row_1 = 0;
 	let player2_row_2 = 0;
 	let player2_row_3 = 0;
-
 	let player2_col_1 = 0;
 	let player2_col_2 = 0;
 	let player2_col_3 = 0;
 
-	let player1_diag_1= 0;
-	let player1_diag_2= 0;
+	let player1_diag_1 = 0;
+	let player1_diag_2 = 0;
 
-	let player2_diag_1= 0;
-	let player2_diag_2= 0;
+	let player2_diag_1 = 0;
+	let player2_diag_2 = 0;
+	
+	let draw,finished,winner; 
 
-	let draw; 
-	let finished;
+	$(".box").css("pointer-events", "none");
+
 		$(".box").click(function(){
 			if (count <= 9) {
 				count+=1;
@@ -59,29 +61,52 @@ $(document).ready(function(){
 				
 
 				if (player1_row_1 == 3 || player1_row_1 == 3 || player1_row_2 == 3 || player1_row_3 == 3 || player1_col_1 == 3 || player1_col_2 == 3 || player1_col_3 == 3 || player1_diag_1 == 3 || player1_diag_2 == 3) {
-					alert("Player1 is winner")
+					winner="<p>Congratulations!! 🎉🎉 <strong>Player 1 is the winner</strong></p>"
 					draw=false
 					finished = true
 				}
 				else if(player2_row_1 == 3 || player2_row_1 == 3 || player2_row_2 == 3 || player2_row_3 == 3 || player2_col_1 == 3 || player2_col_2 == 3 || player2_col_3 == 3 || player2_diag_1 == 3 || player2_diag_2 == 3) {
-						alert("Player2 is winner")
-						draw=false
-						finished = true
+					winner="<p>Congratulations!! 🎉🎉 <strong>Player 2 is the winner</strong></p>"
+					draw=false
+					finished = true
 						
 				}
 				else if (count >= 9){
 					draw=true
 					finished = true
 				}
-				if (draw) {alert("Its a draw!!")}
+				if (draw) {winner="<p>Its a draw!!<strong>Play Again.</strong></p>"}
 			}
 		if (finished) {
 			$(".restart").show()
+			$("#player_turn_displayer").hide()
+			$("#result").html(winner)
+			$("#result").show()
+			$(".box").css("pointer-events", "none");
+
 		}
 
 		})
 
-		$(".restart").click(function(){
+		//for statrting game
+		$("#start").click(function(){
+			$(".box").css("pointer-events", "auto");
+			$(".player1").show()
+			$("#start").hide()
+			
+		});
+		//for restatrting the once finished
+		$("#restart").click(function(){
             location.reload(true);
+        });
+        //for showinf player token on hover over the box
+        $(".box").mouseenter(function(){
+        	if ($(this).attr("class") == "box disabled1" || $(this).attr("class") == "box disabled2"){
+        		$(this).attr('title', "Already Selected")
+        	}else{
+        		let turn_text = count % 2 == 0?"Player1":"Player2"
+           		$(this).attr('title', turn_text)
+        	}
+        	
         });
 });
