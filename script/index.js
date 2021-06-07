954dac06e18f0b8cfb43f02df149a0cf9eb5be42
Naren404 <narendrasunar47@gmail.com>
2021-06-07 18:04:36 +0545

    used only one function to test result

--- script/index.js
+++ script/index.js
@@ -1,7 +1,6 @@
-$(".game-board").hide();
 var clickCount = 0;
 var currentPlayerToken = "O";
-var gameSize,result=false;
+var gameSize,winner=false;
 startGame();
 
 function startGame() {
@@ -12,8 +11,10 @@ function startGame() {
     });
 }
 function setGameSize() {
-    
-    if ($("#gameSize").val() >= 3 && $("#gameSize").val() <= 100) {
+    var minimumGameSize = 3;
+    var maximumGameSize = 100;
+
+    if ($("#gameSize").val() >= minimumGameSize && $("#gameSize").val() <= maximumGameSize) {
         gameSize = parseInt($("#gameSize").val());
     } else {
         alert("Please choose Game Size from 3 to 100.");
@@ -22,27 +23,24 @@ function setGameSize() {
     
 }
 function setGameArena() { 
-    $("#startGame").hide();
-    $("#gameSize").hide();
-    $(".game-board").show();
-    $(".player1").show();
+    $("#startGame,#gameSize").hide();
+    $(".game-board,.player1").show();
  }
  function createAllBoxes(){
-    for (row = 1; row <= gameSize; row++) {
-        row_num = `Row${row}`;
+    var row,col,primaryDiag,secondaryDiag;
+    for ( row = 1; row <= gameSize; row++) {
         $(".rows").append(`<div class="row" data-line=${row}></div>`)
         for ( col = 1; col<= gameSize; col++){
             if (row==col) {
                 primaryDiag = "pDiagonal"; 
             }else{
-                primaryDiag="None";
+                primaryDiag = "none";
             }
             if (col+row == gameSize + 1) {
-                secondaryDiag = "sDiagonal";
+                 secondaryDiag = "sDiagonal";
             }else{
-                secondaryDiag = "None";
+                secondaryDiag = "none";
             }
-            col_num = `Col${col}`;
         $(`[data-line = ${row}]`).append(`<div class="box" data-row=${row} data-col=${col} data-primaryDiag=${primaryDiag} data-secondaryDiag=${secondaryDiag} ></div>`);
         }
     }
@@ -53,11 +51,13 @@ function setGameArena() {
 $(".rows").on("click",".box",function() {
     clickCount++;
     $(this).css("pointer-events", "none");
+
     currentPlayerToken = setCurrentPlayer(clickCount);
     setCurrentPlayerBox(currentPlayerToken,$(this));
     showPlayerTurn();
-    totalMinimumMovesToFindResult = gameSize * 2 -1;
-    
+
+    var totalMinimumMovesToFindResult = gameSize * 2 -1;
+
     if (clickCount >= totalMinimumMovesToFindResult){
         checkForTheResult($(this));
     }
@@ -66,6 +66,7 @@ $(".rows").on("click",".box",function() {
 function setCurrentPlayer(clickCount) {
     return(clickCount % 2 != 0?currentPlayerToken = "O":currentPlayerToken  = "X");
 }
+
 function setCurrentPlayerBox(currentPlayerToken,thisBox){
     currentPlayerToken == "O"?boxDisabledClass = "disabled1":boxDisabledClass = "disabled2";
     thisBox.addClass(boxDisabledClass);
@@ -73,38 +74,30 @@ function setCurrentPlayerBox(currentPlayerToken,thisBox){
 }
 
 function checkForTheResult(thisBox){
+    
     currentRowNumber = thisBox.data('row');
     currentColNumber = thisBox.data('col');
-    getCurrentRowTokenText(currentRowNumber);
-    getCurrentColTokenText(currentColNumber);
-    getCurrentDiagonalText(currentRowNumber,currentColNumber);
-    if (clickCount == gameSize * gameSize) {
-        checkDraw();
+
+    getPossibleWinningTokenTexts(currentRowNumber,currentColNumber);
+
+    if (clickCount == gameSize * gameSize && !winner) {
+        var message = "<p>It's a Draw!! <strong> Play Again.</strong></p>"
+        endTheGame(message);
     }
     
 }
-function getCurrentRowTokenText(currentRowNumber){
-    currentRow = document.querySelectorAll(`div[data-row='${currentRowNumber}']`);
-    testTextToken = generateTokenText(currentRow);
-    checkWinner(testTextToken);
-}
-function getCurrentColTokenText(currentColNumber){
-    currentCol = document.querySelectorAll(`div[data-col='${currentColNumber}']`);
-    testTextToken = generateTokenText(currentCol);
-    checkWinner(testTextToken);
-}
-function getCurrentDiagonalText(currentRowNumber,currentColNumber) {
-    if (currentRowNumber == currentColNumber) {
-        currentDiagonal=document.querySelectorAll(`div[data-primaryDiag='pDiagonal']`);
-    }else if(parseInt(currentRowNumber) + parseInt(currentColNumber) == gameSize + 1){
-        currentDiagonal=document.querySelectorAll(`div[data-secondaryDiag='sDiagonal']`);
-    }   
 
-    if (currentDiagonal){
-        testTextToken = generateTokenText(currentDiagonal);
-        checkWinner(testTextToken);
-    }
+function getPossibleWinningTokenTexts(currentRowNumber,currentColNumber) {
+    
+    currentRow = document.querySelectorAll(`div[data-row='${currentRowNumber}']`);
+    currentCol = document.querySelectorAll(`div[data-col='${currentColNumber}']`);
+    currentPrimaryDiagonal=document.querySelectorAll(`div[data-primaryDiag='pDiagonal']`);
+    currentSecondaryDiagonal=document.querySelectorAll(`div[data-secondaryDiag='sDiagonal']`);
+
+    possibleWinningTokenTexts=[generateTokenText(currentRow),generateTokenText(currentCol),generateTokenText(currentPrimaryDiagonal),generateTokenText(currentSecondaryDiagonal)];
+    checkWinner(possibleWinningTokenTexts);
 }
+
 
 function generateTokenText(currentCells) {
     testText="";
@@ -113,19 +106,13 @@ function generateTokenText(currentCells) {
     });
     return testText;
 }
-function checkWinner(currentTestToken) {
-    if(currentTestToken == currentPlayerToken.repeat(gameSize)){
-        result = true;
+function checkWinner(possibleWinningTokenTexts) {
+    if(possibleWinningTokenTexts.indexOf(currentPlayerToken.repeat(gameSize)) > -1){
+        winner = true;
         message=`<p>Congratulations!! 🎉🎉 <strong> Player "${currentPlayerToken}" is the winner.</strong></p>`;
         endTheGame(message);
     }
 }
-function checkDraw() { 
-    if (!result){
-        message="<p>It's a Draw!! <strong> Play Again.</strong></p>"
-        endTheGame(message);
-    }
- }
 
 function endTheGame(message){
     restartGame();
@@ -148,7 +135,6 @@ function restartGame(){
 }
 
 function showPlayerTurn(){
-    $(".player1").toggle();
-    $(".player2").toggle();
+    $(".player1,.player2").toggle();
 }
 
